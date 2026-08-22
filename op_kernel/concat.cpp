767c2206bbb9bbfd6bcd8c8c82b1b7ee318e08c7
Comb1e <2576131468@qq.com>
2026-08-22 14:14:59 +0800

    perf: reuse decoded concat data pointers

--- op_kernel/concat.cpp
+++ op_kernel/concat.cpp
@@ -76,7 +76,7 @@ __aicore__ inline void ProcessByRows(ListTensorDesc& inputs, GlobalTensor<uint8_
         }
         const uint64_t segmentBytes = desc.GetShape(tilingData.concatDim) * innerSize * tilingData.elementBytes;
         GlobalTensor<uint8_t> source;
-        source.SetGlobalBuffer(inputs.GetDataPtr<uint8_t>(inputIdx));
+        source.SetGlobalBuffer(desc.GetDataPtr());
 
         uint64_t copied = 0;
         while (copied < segmentBytes) {
@@ -139,7 +139,7 @@ __aicore__ inline void ProcessByChunks(ListTensorDesc& inputs, GlobalTensor<uint
             (blockIdx + blockCount - globalChunkBase % blockCount) % blockCount;
 
         GlobalTensor<uint8_t> source;
-        source.SetGlobalBuffer(inputs.GetDataPtr<uint8_t>(inputIdx));
+        source.SetGlobalBuffer(desc.GetDataPtr());
         for (uint64_t workItem = firstWorkItem; workItem < inputWorkItems; workItem += blockCount) {
             const uint64_t outer = workItem / chunkCount;
             const uint64_t chunk = workItem - outer * chunkCount;
